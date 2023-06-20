4dcbdc1668df0e3a9686f9a48167ba170b231d13
a101201031 <a101201031@gmail.com>
2023-06-21 02:23:27 +0900

    init: project init

---
+++ packages/prototype/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), eslint(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+});
