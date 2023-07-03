4d645f1eadbf4ae1f391105f70e0412a8ead3611
a101201031 <a101201031@gmail.com>
2023-07-03 23:36:20 +0900

    feat: add wait for auth to load

--- packages/frontend/src/page/Base.tsx
+++ packages/frontend/src/page/Base.tsx
@@ -1,12 +1,17 @@
 import { CssBaseline } from '@mui/material';
 import { TopAppBar } from 'component';
 import { Outlet } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { isCredentialLoadedAtom } from 'store';
 
 export function Base() {
+  const isLoaded = useRecoilValue(isCredentialLoadedAtom);
   return (
-    <CssBaseline>
-      <TopAppBar />
-      <Outlet />
-    </CssBaseline>
+    isLoaded && (
+      <CssBaseline>
+        <TopAppBar />
+        <Outlet />
+      </CssBaseline>
+    )
   );
 }
