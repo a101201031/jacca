e5fdc1fa0bfb7c5e54b20b4208e86fae687221f5
a101201031 <a101201031@gmail.com>
2023-11-10 19:49:29 +0900

    style: add font

--- packages/frontend/src/style/global.ts
+++ packages/frontend/src/style/global.ts
@@ -2,10 +2,31 @@ import { css } from '@emotion/react';
 
 export const globalStyle = css`
   @font-face {
-    font-family: 'Pretendard-Regular';
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-display: swap;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff')
+      format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-display: swap;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff')
+      format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
     src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
       format('woff');
     font-weight: 400;
     font-style: normal;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 300;
+    font-display: swap;
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Light.woff')
+      format('woff');
   }
 `;
