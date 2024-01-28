bee6499876bc92573c54d7b7bcb76158344645d3
a101201031 <a101201031@gmail.com>
2024-01-28 10:38:05 +0900

    feat: add all exception filter

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,13 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config';
+import { AllExceptionsFilter } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig = app.get(AppConfigService);
+  const httpAdapter = app.get(HttpAdapterHost);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   await app.listen(appConfig.port);
 }
