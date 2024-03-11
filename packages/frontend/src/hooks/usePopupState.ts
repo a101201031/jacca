e4195f92dc3819e74be705b8a97461d66806aeab
a101201031 <a101201031@gmail.com>
2024-03-11 22:05:22 +0900

    refactor: separation of concerns (page/Cafe)

---
+++ packages/frontend/src/hooks/usePopupState.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from 'react';
+
+export function usePopupState(initialState = false) {
+  const [isOpen, setIsOpen] = useState(initialState);
+
+  const open = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const close = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  return { isOpen, open, close };
+}
